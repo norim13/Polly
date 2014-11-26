13d39ee17a265a6a6935378688cf10b9abe93ad1
ei12021 <ei12021@fe.up.pt>
2014-11-26 23:30:55 +0000

    so aceita polls com 2 ou mais options

--- new_poll.php
+++ new_poll.php
@@ -8,8 +8,12 @@
 	<form id="new_poll_form" action="new_poll_action.php" method="post">
 		Poll Title:<input type="text" name="title"><br><br>
 		Poll Description:<input type="text" name="description"><br><br>
-		
+		<input type="button" id="addOption"  value="Add Poll Option" />
 		<input type="submit" name="submit" />
+		<script type="text/javascript">
+ 	addOptionRequest();
+	addOptionRequest();
+	</script>
 		
 	</form>
 </div>
--- new_poll_action.php
+++ new_poll_action.php
@@ -11,24 +11,46 @@
 		$title = htmlspecialchars($_POST['title']);
 		$description = htmlspecialchars($_POST['description']);
 		$userId = getUserIDbyUsername($_SESSION['username']);
+		$options = $_POST['option'];
+
+		//verifies if all the options are filled
+		foreach($options as $option){
+			if($option=='')
+			{
+				//por um pop up
+				header("Location: new_poll.php");
+				return;
+			}
+		}
+
+		//adds the poll
 		if($title !=''&& $description !='')
 		{
 			$stmt = $db->prepare('INSERT INTO poll(id,title,description,userId) VALUES (?,?,?,?)');
 			$stmt->execute(array(NULL,$title,$description,$userId));
 		}
+		else
+		{
+			//por um pop up
+			header("Location: new_poll.php");
+			return;
+		}
+
+		$poll = getPollByTitle($title);
+		$poll_id = $poll['id'];
+		
+	//	adds the options to the poll
+		foreach($options as $option){
+			$optionT = htmlspecialchars($option);
+			$db->exec("INSERT INTO pollOption VALUES(NULL,'$poll_id', '$optionT', 0)");	
+		}
+		echo 'fim';
+		header("Location:polls_all.php");
+
 	}
 ?>
 
 
-<div class="basePanel">
-	<h3>Form Title: <?=$title?></h3>
-	<p>Form Description: <?=$description?></p>
-	<form id="add_options_poll" action="add_poll_options.php" method="post">
-		<input type="hidden" name="title" value="<?=$title?>" />
-		<input type="button" id="addOption"  value="Add Poll Option" />
-		<input type="submit" name="submit" />
-	</form>
-</div>
 
 
 <?	include('templates/footer.php'); ?>
