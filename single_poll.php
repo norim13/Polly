0464e10c27109bbf442fe1132499fbcb0692ef82
ei12022 <ei12022@fe.up.pt>
2014-11-30 16:18:08 +0000

    fixed menu bar

--- single_poll.php
+++ single_poll.php
@@ -2,12 +2,12 @@
 	include('templates/header.php');
 	include('database/polls_fetch.php');
 	include('pollgoogle.php');
-	include('getPollURL.php');
+	include_once('getPollURL.php');
 
 	$poll_hash = $_GET['poll'];
 	$poll = getPollByHash($poll_hash);
 ?>	
-	<div  class="poll_item_stat" style="width: 90%; overflow: hidden;">
+	<div  class="poll_item" style="overflow: hidden;">
 		<script>alert(<?=$poll_hash?>)</script>
 		<?
 			$link = getPollUrl($poll_hash);
